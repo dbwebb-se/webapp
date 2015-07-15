fee98cc08076b3ac90a45fd65e32923a60ea6a98
Olund <henke.olund@gmail.com>
2015-07-15 14:48:56 +0200

    Director router + mustache works pretty well

--- example/realApp/js/app.js
+++ example/realApp/js/app.js
@@ -4,14 +4,22 @@
 
 var app = (function() {
 
-    var home = function(name) {
-        renderTemplate('home', { name: name || 'anonymous' });
+    var hello = function(name) {
+        name = name || 'anonymous';
+
+        renderTemplateFromId('home', {
+            name: name
+        });
     }
+
     var author = function () { console.log("author"); };
+
     var books = function () { console.log("books"); };
+
     var viewBook = function (bookId) {
         console.log("viewBook: bookId is populated: " + bookId);
     };
+
     var getExampleNames = function() {
         // get view.
         $.get('views/examples.mustache', function(template) {
@@ -30,11 +38,16 @@ var app = (function() {
         var url = "http://localhost:1337/api/v1/examples/" + slug;
         $.get(url, function (data) {
             console.log(data);
+
+            $('#view').html(data);
         });
     };
 
     var routes = {
-        '/': home,
+        '/': function() { // TODO: FIX THIS.
+            renderTemplateFromId('about', { title: 'Home', text: 'lorem' });
+        },
+        '/hello/?:name': hello,
         '/author': author,
         '/books': [ books, function() {
             var view = {
@@ -45,7 +58,7 @@ var app = (function() {
         } ],
         '/books/view/:bookId': viewBook,
         '/examples': getExampleNames,
-        '/examples/:slug': examples
+        '/examples/:slug': examples,
     };
 
     /**
@@ -56,15 +69,18 @@ var app = (function() {
     var renderTemplateFromId = function(templateId, obj) {
         var templateHTML = document.getElementById(templateId).innerHTML;
         var div = document.getElementById('view');
-
         div.innerHTML = Mustache.render(templateHTML, obj);
     };
+
     var renderTemplate = function(template, obj) {
         var div = document.getElementById('view');
         div.innerHTML = Mustache.render(template, obj);
     };
 
-    var router = new Router(routes);
+    var options = {
+        strict: false,
+    }
+    var router = new Router(routes).configure(options);
     router.init();
     var privates = {
 
@@ -72,74 +88,4 @@ var app = (function() {
 
     return {};
 })();
-
-
-
-
-
-    // The routes for the app, a self invoking anon function..
-    /*routes: (function() {
-
-        route('/', 'home', function() {
-
-            this.name = 'Anonymous';
-            console.log('Home route');
-        });
-
-        route('/about', 'about', function() {
-            console.log('About route');
-            this.title = 'About';
-            this.text = 'yoloasdasjdlkjaslkdjasl';
-        });
-
-        route('/ajax', 'ajaxLoading', function() {
-            this.heading = 'ajax loading';
-            this.dataBody = 'Loading....';
-
-            var root = 'http://jsonplaceholder.typicode.com';
-            // A real ajax call.
-            $.ajax({
-                url: root + '/posts/1',
-                method: 'GET'
-            }).done(function(data) {
-
-                $('#view').html(Mustache.render($('#ajaxLoading').html(), {
-                    heading: 'Ajax load',
-                    dataBody: data.body
-                }));
-
-            }).fail(function() {
-                alert("error loading json data");
-            });
-        });
-
-        route('/examples', function() {
-            // Load example template from file.
-            var data = [ "a", "b", "c", "d" ];
-            this.list = data;
-            var view = $('#view');
-
-            // get view.
-            $.get('views/examples.mustache', function(template) {
-                console.log('Tepmlate', template);
-                // get data.
-                $.get('http://localhost:1337/api/v1/examples', function(data) {
-                    console.log('data', data);
-                    var output = Mustache.render(template, {
-                        list: data
-                    });
-                    view.html(output);
-                });
-            });
-        });
-
-        route('/examples/detectScreenSize', 'screensize', function () {
-            var url = 'http://localhost:1337/api/v1/examples/detectScreenSize';
-            $.get(url, function (data) {
-                console.log(data);
-                $('#view').html(data);
-            });
-        });
-
-    })(),*/
 
