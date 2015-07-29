b2bfc39c17c730faaad7354e2252e09f2bb4f37e
Olund <henke.olund@gmail.com>
2015-07-29 15:39:11 +0200

    Settings js

---
+++ example/realApp/js/settings.js
@@ -0,0 +1,7 @@
+(function() {
+    // Disable jquery mobile navigation system.
+    $.mobile.ajaxEnabled = false;
+    $.mobile.linkBindingEnabled = false;
+    $.mobile.hashListeningEnabled = false;
+    $.mobile.pushStateEnabled = false;
+})();
