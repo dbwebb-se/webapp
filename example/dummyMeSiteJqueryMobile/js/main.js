4e1f3b7bd60ad601513a9f298afb5e67a711e425
Olund <henke.olund@gmail.com>
2015-08-03 15:22:58 +0200

    shared navigation done

--- example/dummyMeSiteJqueryMobile/js/main.js
+++ example/dummyMeSiteJqueryMobile/js/main.js
@@ -1,5 +1,11 @@
 (function() {
     "use strict";
+
+    // Add shared header, footer as a toolbar.
+    $("[data-role='header'], [data-role='footer']").toolbar();
+
+
+
     if (window.location.href.indexOf("index") > -1) {
         document.getElementById("middleSpan").innerHTML = "<h3>This text is passed from main.js. If you click the links, you can view some images.</h3>";
     } else if (window.location.href.indexOf("genie") > -1) {
@@ -12,3 +18,22 @@
         genie.appendChild(cap);
     }
 })();
+
+$(document).on('pagecontainerchange', function() {
+
+    // Each of the pages in this demo has a data-title attribute
+    // which value is equal to the text of the nav button
+    // For example, on first page: <div data-role="page" data-title="Me-sida">
+    var current = $( ".ui-page-active" ).jqmData("title");
+
+    // Change the heading
+    $("[data-role='header'] h1").text(current);
+    //Remove active class from nav buttons
+    $("[data-role='navbar'] a.ui-btn-active").removeClass("ui-btn-active");
+    //Add active class to current nav button
+    $("[data-role='navbar'] a").each(function() {
+        if ($(this).text() === current) {
+            $(this).addClass( "ui-btn-active");
+        }
+    });
+});
