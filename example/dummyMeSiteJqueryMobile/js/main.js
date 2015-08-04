3a25bbdd835c7bfd73d88873b76e0334e0121759
Olund <henke.olund@gmail.com>
2015-08-04 12:10:31 +0200

    Icon now working for the menu

--- example/dummyMeSiteJqueryMobile/js/main.js
+++ example/dummyMeSiteJqueryMobile/js/main.js
@@ -1,28 +1,8 @@
 (function () {
-    "use strict";
-    // Add shared header as a toolbar.
-    $("[data-role='header']").toolbar();
-
+    'use strict';
     // Add shared panel as a panel.
-    $( "body>[data-role='panel']" ).panel();
-
+    $('#menu').panel().enhanceWithin();
+    // Add shared header as a toolbar.
+    $("[data-role='header']").toolbar().enhanceWithin();
 })();
-/*
-$(document).on('pagecontainerchange', function () {
 
-    // Each of the pages in this demo has a data-title attribute
-    // which value is equal to the text of the nav button
-    // For example, on first page: <div data-role="page" data-title="Me-sida">
-    var current = $('.ui-page-active').jqmData('title');
-
-    // Change the heading
-    //$("[data-role='header'] h1").text(current);
-    //Remove active class from nav buttons
-    $("[data-role='navbar'] a.ui-btn-active").removeClass('ui-btn-active');
-    //Add active class to current nav button
-    $("[data-role='navbar'] a").each(function () {
-        if ($(this).text() === current) {
-            $(this).addClass('ui-btn-active');
-        }
-    });
-});*/
