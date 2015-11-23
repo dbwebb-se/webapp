f96efae8434fc094272f7d250deaf0e686493a9e
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-11-23 12:51:39 +0100

    prepare for kmom03

--- example/arbetsformedlingen/af.bash
+++ example/arbetsformedlingen/af.bash
@@ -4,6 +4,6 @@ curl \
     --header "Accept: application/json" \
     --header "Accept-Language: sv" \
     --header "From: mos@dbebb.se" \
-    http://api.arbetsformedlingen.se/af/v0/$* \
+    "http://api.arbetsformedlingen.se/af/v0/$*" \
     | python3 -mjson.tool
 #    | jq  '.'
