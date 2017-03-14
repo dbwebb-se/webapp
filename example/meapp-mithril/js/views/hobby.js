d2ae455b07830d4f53267e75372f550f58e923b5
Emil Folino <emil.folino@bth.se>
2017-03-14 11:08:46 +0100

    Added meapp exercise code for kmom01

---
+++ example/meapp-mithril/js/views/hobby.js
@@ -0,0 +1,10 @@
+var m = require("mithril")
+
+module.exports = {
+    view: function() {
+        return [
+            m("h1", "My hobby"),
+            m("p", "I run orienteering most of the time. And was pretty good at it before I blew my knee.")
+        ]
+    }
+}
---
+++ example/meapp-mithril/js/views/layout.js
@@ -0,0 +1,18 @@
+var m = require("mithril")
+
+module.exports = {
+    view: function(vnode) {
+        return m("main", [
+            m("navbar", [
+                m("div.container", [
+                    m("h2.brand", "Emil Folino"),
+                    m("ul.nav", [
+                        m("li", [m("a", {href: "/", oncreate: m.route.link}, "Me")]),
+                        m("li", [m("a", {href: "/hobby", oncreate: m.route.link}, "Hobby")])
+                    ])
+                ])
+            ]),
+            m("section.container", vnode.children)
+        ])
+    }
+}
