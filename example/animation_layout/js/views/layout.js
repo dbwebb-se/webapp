6ac2d1df3ebacd4b45ab42ac8fbdd8f3ba895051
Emil Folino <emilfolino@gmail.com>
2019-05-16 12:59:31 +0200

    Added bottom nav to example

--- example/animation_layout/js/views/layout.js
+++ example/animation_layout/js/views/layout.js
@@ -4,7 +4,17 @@ import m from 'mithril';
 
 let layout = {
     view: function(vnode) {
-        return m("div.layout", vnode.children);
+        return [
+            m("div.layout", vnode.children),
+            m("footer.footer", [
+                m("a.list-item", {
+                    href: "/detail/1", oncreate: m.route.link
+                }, "Siffran 1"),
+                m("a.list-item", {
+                    href: "/detail/2", oncreate: m.route.link
+                }, "Siffran 2")
+            ])
+        ];
     }
 };
 
