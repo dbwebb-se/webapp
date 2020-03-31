2aa05b7b9eb2dda340ef2bdd521bf350c24245fb
Emil Folino <emilfolino@gmail.com>
2020-03-31 10:54:38 +0200

    Added files for lecture kmom01

---
+++ example/lectures/2020/kmom01/main.js
@@ -0,0 +1,16 @@
+(function IIFE () {
+    var root = document.getElementById('root');
+
+    var container = document.createElement("div");
+
+    container.className = "container";
+
+    var title = document.createElement("h1");
+
+    title.textContent = "Skolor";
+
+    container.appendChild(title);
+    root.appendChild(container);
+
+    // http://api.kolada.se/v2/ou?municipality=1080&title=skola
+})();
