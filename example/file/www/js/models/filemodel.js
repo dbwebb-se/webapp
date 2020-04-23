434dbfcb114fd82bb8aaf1ffa9eeeca1b1a993a2
Emil Folino <emilfolino@gmail.com>
2020-04-23 07:48:49 +0200

    Finished read/write to file example

--- example/file/www/js/models/filemodel.js
+++ example/file/www/js/models/filemodel.js
@@ -1,14 +1,73 @@
+import m from "mithril";
+
 var fileModel = {
     current: "",
+    file: null,
+    readText: "",
 
-    writeToFile: function() {
-
+    createFile: function() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+            console.log('file system open: ' + fs.name);
+            fs.root.getFile(
+                "data.txt",
+                { create: true, exclusive: false },
+                function (fileEntry) {
+                    fileModel.file = fileEntry;
+                    fileModel.writeToFile(fileModel.file, null, false);
+            }, function() {
+                outputError("Error loading file");
+            });
+        }, function() {
+            outputError("Error loading filesystem");
+        });
     },
 
-    readFromFile: function() {
-        
+    writeToFile: function(fileEntry, data, append) {
+        fileEntry.createWriter(function (fileWriter) {
+            fileWriter.onwriteend = function() {
+                console.log("Successful file write...");
+            };
+
+            fileWriter.onerror = function (e) {
+                console.log("Failed file write: " + e.toString());
+            };
+
+            if (append) {
+                try {
+                    fileWriter.seek(fileWriter.length);
+                }
+                catch (e) {
+                    console.log("file doesn't exist!");
+                }
+            }
+
+            if (data) {
+                fileWriter.write(data);
+            }
+        });
+    },
+
+    readFromFile: function(fileEntry) {
+        fileEntry.file(function (file) {
+            var reader = new FileReader();
+
+            reader.onloadend = function() {
+                console.log(this.result);
+                fileModel.readText = this.result;
+                m.redraw();
+            };
+
+            reader.readAsText(file);
+
+        }, function() {
+            outputError("Error reading from file");
+        });
     }
 
 };
+
+function outputError(errorMessage) {
+    console.error();
+}
 
 export default fileModel;
--- example/file/www/js/views/fileview.js
+++ example/file/www/js/views/fileview.js
@@ -2,15 +2,40 @@ import m from "mithril";
 import fileModel from "../models/filemodel.js";
 
 var fileView = {
+    oninit: fileModel.createFile,
     view: function() {
         return m("div.container", [
             m("h1", "Hello File"),
             m("input.input[type=text]", {
-                oninput: function (e) {
+                oninput: function(e) {
                     fileModel.current = e.target.value
                 },
                 value: fileModel.current
             }),
+            m("button.button", {
+                onclick: function() {
+                    fileModel.writeToFile(
+                        fileModel.file,
+                        fileModel.current,
+                        false
+                    );
+                }
+            }, "Write text to file"),
+            m("button.button", {
+                onclick: function() {
+                    fileModel.writeToFile(
+                        fileModel.file,
+                        fileModel.current,
+                        true
+                    );
+                }
+            }, "Append text to file"),
+            m("button.button", {
+                onclick: function() {
+                    fileModel.readFromFile(fileModel.file);
+                }
+            }, "Read from file"),
+            m("div.read-text", fileModel.readText)
         ]);
     }
 };
