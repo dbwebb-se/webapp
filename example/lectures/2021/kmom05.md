ef79e750553dc5195151643fa0ab371d34489fb2
Emil Folino <emilfolino@gmail.com>
2021-04-28 09:27:54 +0200

    lecture notes for kmom05

--- example/lectures/2021/kmom05.md
+++ example/lectures/2021/kmom05.md
@@ -2,6 +2,14 @@
 
 Hur kan vi bygga en app?
 
+4 alternativ:
+
+* [Progressive Web Apps](https://web.dev/progressive-web-apps/)
+* Web Apps in WebViews - [Cordova](https://cordova.apache.org/) - [Ionic](https://ionicframework.com/)
+* Cross-Compiled Apps - [Xamarin](https://dotnet.microsoft.com/apps/xamarin) - [React Native](https://reactnative.dev/) - [Flutter](https://flutter.dev/)
+* Native - [Xcode](https://developer.apple.com/) - [Android Studio](https://developer.android.com/studio)
+
+
 ## En app på AppStore
 
 StartClock
@@ -12,4 +20,4 @@ StartClock
 
 [Uncle Bob on SRP](https://blog.cleancoder.com/uncle-bob/2014/05/08/SingleReponsibilityPrinciple.html)
 
-
+Exempelprogram: `srp.js`
---
+++ example/lectures/2021/kmom05/srp.js
@@ -0,0 +1,34 @@
+import m from "mithril";
+
+const deliveries = {
+    current: {},
+    save: function save() {
+        deliveries.current["api_key"] = "API_KEY";
+
+        return m.request({
+            url: "URL/deliveries",
+            method: "POST",
+            body: deliveries.current,
+        }).then(function(result) {
+            return m.request({
+                url: `URL/products/${deliveries.current.product_id}`,
+                method: "GET",
+            }).then(function(result) {
+                let product = result.data;
+
+                product["api_key"] = "API_KEY";
+                product["stock"] = product["stock"] + deliveries.current.amount;
+
+                return m.request({
+                    url: "URL/products",
+                    method: "PUT",
+                    body: product,
+                }).then(function() {
+                    console.log("updated");
+                });
+            });
+        }).catch(function(error) {
+
+        });
+    }
+}
