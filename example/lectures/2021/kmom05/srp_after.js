89749cb1c79c10269630357571570baa59f98d87
Emil Folino <emilfolino@gmail.com>
2021-04-28 12:17:17 +0200

    After srp'ifying the functions

---
+++ example/lectures/2021/kmom05/srp_after.js
@@ -0,0 +1,54 @@
+import m from "mithril";
+
+const product = {
+    getProduct: function getProduct(
+        product_id,
+        amount,
+        callback,
+    ) {
+        return m.request({
+            url: `URL/products/${product_id}`,
+            method: "GET",
+        }).then(function(result) {
+            if (callback) {
+                let product = result.data;
+
+                product["api_key"] = "API_KEY";
+                product["stock"] = product["stock"] + amount;
+
+                callback(product);
+            }
+        });
+    },
+    updateProduct: function updateProduct(product) {
+        return m.request({
+            url: "URL/products",
+            method: "PUT",
+            body: product,
+        }).then(function() {
+            console.log("updated");
+        });
+    }
+};
+
+
+const deliveries = {
+    current: {},
+    save: function save() {
+        deliveries.current["api_key"] = "API_KEY";
+
+        return m.request({
+            url: "URL/deliveries",
+            method: "POST",
+            body: deliveries.current,
+        }).then(function(result) {
+            product.getProduct(
+                deliveries.current.product_id,
+                deliveries.current.amount,
+                product.updateProduct,
+            );
+        }).catch(function(error) {
+
+        });
+    }
+}
