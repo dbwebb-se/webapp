bf42283dd6c6563bea5f23aa6870209d8c17cb7b
Emil Folino <emilfolino@gmail.com>
2023-03-06 09:53:54 +0100

    changed to work like example

--- example/web-component-starter/main.js
+++ example/web-component-starter/main.js
@@ -2,10 +2,6 @@ import LagerTitle from "./components/lager-title.js";
 import ProductList from "./components/product-list.js";
 import SingleProduct from "./components/single-product.js";
 
-(function IIFE() {
-    "use strict";
-
-    customElements.define('lager-title', LagerTitle);
-    customElements.define('product-list', ProductList);
-    customElements.define('single-product', SingleProduct);
-})();
+customElements.define('lager-title', LagerTitle);
+customElements.define('product-list', ProductList);
+customElements.define('single-product', SingleProduct);
