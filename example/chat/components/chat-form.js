d99daaa1342d0e6030ac382b95d3ab52ccba7e68
Emil Folino <emilfolino@gmail.com>
2024-02-16 12:49:58 +0100

    Added chat example

---
+++ example/chat/components/chat-form.js
@@ -0,0 +1,49 @@
+export default class ChatForm extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `<textarea class="message-textarea" id="message-textarea"></textarea>
+
+        <button class="message-button" id="message-button">Send</button>
+
+        <h2>Sent messages</h2>
+        <div id="messages"></div>`;
+
+        window.addEventListener("load", () => {
+            this.init();
+        }, false);
+    }
+
+    init() {
+        const socket = io("https://lager-chat.emilfolino.se");
+
+        const textarea = document.getElementById("message-textarea");
+        const button = document.getElementById("message-button");
+        const messages = document.getElementById("messages");
+
+        button.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            if (textarea.value) {
+                socket.emit('chat message', textarea.value);
+                textarea.value = '';
+            }
+        });
+
+        socket.on('chat message', (msg) => {
+            const item = document.createElement('p');
+
+            item.textContent = msg;
+
+            messages.prepend(item);
+        });
+    }
+}
+
+
+
+
+
+
---
+++ example/chat/main.js
@@ -0,0 +1,3 @@
+import ChatForm from "./components/chat-form.js";
+
+customElements.define("chat-form", ChatForm);
